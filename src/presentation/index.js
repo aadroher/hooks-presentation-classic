fda318c69ee4d475615f74fa154f8406734b14af
Armand Adroher <aadroher@gmail.com>
2019-08-14 01:00:48 +0100

    Adds basic draft.

--- src/presentation/index.js
+++ src/presentation/index.js
@@ -21,10 +21,13 @@ import {
   Heading,
   ListItem,
   List,
+  Link,
   Quote,
   Slide,
   Text,
   S,
+  Layout,
+  Fit,
 } from 'spectacle';
 
 // Import theme
@@ -65,7 +68,14 @@ const Presentation = () => (
       </Text>
     </Slide>
     <Slide>
-      <Heading>Full disclosure</Heading>
+      <Heading size={4} textColor="tertiary" textAlign="left">
+        Full disclosure
+      </Heading>
+      <List>
+        <ListItem>🐥 No expert whatsoever.</ListItem>
+        <ListItem>🧠 A topic for a discussion.</ListItem>
+        <ListItem>🙋 Please help me!</ListItem>
+      </List>
     </Slide>
     <Slide>
       <Heading size={4} textColor="tertiary" textAlign="left">
@@ -78,6 +88,7 @@ const Presentation = () => (
           without using class components.
         </ListItem>
       </List>
+      <Text> f: (x ↦ y = f(x)) ⇒ 🚀</Text>
     </Slide>
     <Slide>
       <Heading size={4} textColor="tertiary" textAlign="left">
@@ -91,8 +102,8 @@ const Presentation = () => (
         <ListItem>
           They involve the use of <Code>this</Code>.
         </ListItem>
-        <ListItem>Code is organised by the lifecycle methods.</ListItem>
-        <ListItem>Poor option to manage the state.</ListItem>
+        <ListItem>Code is organised by lifecycle method.</ListItem>
+        <ListItem>🍝 Poor option to manage the state.</ListItem>
       </List>
     </Slide>
     <Slide>
@@ -108,7 +119,7 @@ const Presentation = () => (
     </Slide>
     <Slide>
       <Heading size={4} textColor="tertiary" textAlign="left">
-        "Classes" in Javascript?
+        "Classes" in Javascript
       </Heading>
       <CodePane
         theme="light"
@@ -151,25 +162,9 @@ const Presentation = () => (
       />
     </Slide>
     <Slide>
-      <Heading size={4} textColor="tertiary" textAlign="left">
-        Deeply nested
+      <Heading size={3} textColor="tertiary" fit>
+        🎣 Basic hooks
       </Heading>
-    </Slide>
-    <Slide>
-      <Heading size={4} textColor="tertiary" textAlign="left">
-        Basic hooks
-      </Heading>
-      <List>
-        <ListItem>
-          <Code>useState</Code>
-        </ListItem>
-        <ListItem>
-          <Code>useEffect</Code>
-        </ListItem>
-        <ListItem>
-          <Code>useContext</Code>
-        </ListItem>
-      </List>
     </Slide>
     <Slide>
       <Heading size={4} textColor="tertiary" textAlign="left">
@@ -194,7 +189,41 @@ const Presentation = () => (
       />
     </Slide>
     <Slide>
+      <Heading fit>💪 See them in action</Heading>
+    </Slide>
+    <Slide>
+      <Heading size={4} textColor="tertiary" textAlign="left">
+        Additional hooks
+      </Heading>
+      <Layout>
+        <List padding="0 16px" fit>
+          <ListItem>useContext</ListItem>
+          <ListItem>useReducer</ListItem>
+          <ListItem>useCallback</ListItem>
+          <ListItem>useMemo</ListItem>
+        </List>
+        <List padding="0 32px" fit>
+          <ListItem>useRef</ListItem>
+          <ListItem>useImperativeHandle</ListItem>
+          <ListItem>useLayoutEffect</ListItem>
+          <ListItem>useDebugValue</ListItem>
+          <ListItem>... roll you own! 🎉</ListItem>
+        </List>
+      </Layout>
+    </Slide>
+    <Slide>
+      <Heading fit>🤔 Any thoughts? </Heading>
+    </Slide>
+    <Slide>
       <Heading>Thanks!</Heading>
+      <Text margin="10px 0 0" textColor="tertiary" size={1}>
+        <Link href="https://twitter.com/aadroher">@aadroher</Link>
+      </Text>
+      <Text margin="10px 0 0" textColor="tertiary" size={1}>
+        <Link href="https://armandadroher.name">
+          https://armandadroher.name
+        </Link>
+      </Text>
     </Slide>
   </Deck>
 );
